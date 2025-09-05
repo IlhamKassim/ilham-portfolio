da019eed29c619c33d59a5c9d3fa6079b7c9fe17
hamboii <hamboii@users.noreply.github.com>
2025-09-05 00:25:08 -0400

    Update Image

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    '@next/next/no-img-element': 'warn',
-    'no-console': 'warn',
-    'prefer-const': 'error',
+const { FlatCompat } = require('@eslint/eslintrc')
+const js = require('@eslint/js')
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+})
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'dist/**', 'build/**'],
   },
-}
+  ...compat.extends('next/core-web-vitals'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      '@next/next/no-img-element': 'warn',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+    },
+  },
+]
